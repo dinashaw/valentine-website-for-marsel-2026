57568d30b060d641956f7e07abfc1078db835a01
dinashaw <dina12522@gmail.com>
2026-02-08 22:38:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ window.addEventListener('DOMContentLoaded', () => {
     validateConfig();
 
     // Set texts from config
-    document.getElementById('valentineTitle').textContent = `${config.valentineName}, любовь моя...`;
+    document.getElementById('valentineTitle').textContent = `${config.valentineName}, любимый`;
     
     // Set first question texts
     document.getElementById('question1Text').textContent = config.questions.first.text;
